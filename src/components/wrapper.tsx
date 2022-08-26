3dbce55441a47ff961296385b17dd10dc00f86d6
deger4beger <deger.begerrr@gmail.com>
2022-08-27 00:37:04 +0600

    -latest

--- src/components/wrapper.tsx
+++ src/components/wrapper.tsx
@@ -22,15 +22,11 @@ const Wrapper = ({ children }: {
 }
 
 const Header = () => {
-	// const { switchTheme } = useTheme()
 	return (
 		<div className="flex justify-center p-2 bg-gray-700 relative">
 	    <div>
 	    	Take note that this is experimental version <span className="underline">0.1</span>
 	    </div>
-	    {/*<div className="absolute top-2 right-4 cursor-pointer select-none" onClick={switchTheme}>
-	    	 🌓🌑🌕🌘🌖
-	    </div>*/}
 	  </div>
 	)
 }
